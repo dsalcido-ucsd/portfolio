bda0f2b9bab40c506e6bf25cac1364a57e3b7886
dsalcido-ucsd <dsalcido@ucsd.edu>
2025-11-07 19:07:15 -0800

    display stats

--- meta/main.js
+++ meta/main.js
@@ -13,4 +13,77 @@ async function loadData() {
   return data;
 }
 
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/dsalcido-ucsd/dsalcido-ucsd.github.io/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,
+        configurable: true,
+        writable: false,
+      });
+
+      return ret;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  const numFiles = d3.group(data, d => d.file).size;
+  dl.append('dt').text('Number of files');
+  dl.append('dd').text(numFiles);
+
+  const fileLengths = d3.rollups(
+    data,
+    (v) => d3.max(v, (v) => v.line),
+    (d) => d.file,
+  );
+  const averageFileLength = d3.mean(fileLengths, (d) => d[1]);
+  dl.append('dt').text('Average file length');
+  dl.append('dd').text(Math.round(averageFileLength) + ' lines');
+
+  const longestFile = d3.greatest(fileLengths, (d) => d[1]);
+  dl.append('dt').text('Longest file');
+  dl.append('dd').text(`${longestFile[0]} (${longestFile[1]} lines)`);
+
+  const averageLineLength = d3.mean(data, d => d.length);
+  dl.append('dt').text('Average line length');
+  dl.append('dd').text(Math.round(averageLineLength) + ' characters');
+
+  const workByPeriod = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }),
+  );
+  const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+  dl.append('dt').text('Most work done');
+  dl.append('dd').text(maxPeriod);
+}
+
 let data = await loadData();
+let commits = processCommits(data);
+console.log(commits);
+
+renderCommitInfo(data, commits);
